12b37889e6a41d1657bea9a210c2668c52bbbe26
sang1kev <sang1kev@naver.com>
2025-07-10 10:21:51 +0900

    [Add] UIHandler study

---
+++ Assets/01. Data Structure/02. Script/UIHandler/UIHandler.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIHandler : MonoBehaviour, IPointerDownHandler, IDragHandler
+{
+    private RectTransform parentRect;
+
+    private Vector2 basePos;
+    private Vector2 startPos;
+    private Vector2 moveOffset;
+
+    void Awake()
+    {
+        parentRect = transform.parent.GetComponent<RectTransform>();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        moveOffset = eventData.position - startPos;
+        parentRect.anchoredPosition = basePos + moveOffset;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        basePos = parentRect.anchoredPosition;
+        startPos = eventData.position;
+    }
+}
